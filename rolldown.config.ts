f4f8788e14113b7e1359591929bb0d749c13225c
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2025-05-05 18:08:44 +0200

    Added tsdown and rolldown

--- rolldown.config.ts
+++ rolldown.config.ts
@@ -1,14 +1,19 @@
 import { cancel, multiselect } from "@clack/prompts";
 import { assertPath } from "@monorepo-starter/utils";
 import { join } from "node:path";
-import { defineConfig, RolldownOptions } from "rolldown";
+import { build, RolldownOptions } from "rolldown";
 
 const monorepoRoot = import.meta.dirname;
 
 const packagesDir = join(monorepoRoot, "packages");
 
 const packagesList: PackageOptions[] = [
-  "utils",
+  {
+    name: "utils",
+    rollDownOpts: {
+      external: ["fast-glob"],
+    },
+  },
   {
     name: "scripts",
     entrypoint: "src/init_repo.ts",
@@ -18,19 +23,6 @@ const packagesList: PackageOptions[] = [
   },
 ];
 
-const buildTargets = await multiselect({
-  message: "Choose the packages to build:",
-  options: await genRolldownOptions(packagesList),
-  required: false,
-});
-
-if (!Array.isArray(buildTargets) || !buildTargets.length) {
-  cancel("Operation canceled.");
-  process.exit(1);
-}
-
-export default defineConfig([...buildTargets]);
-
 type PackageOptions = string | PackageData;
 
 type PackageData = {
@@ -39,35 +31,64 @@ type PackageData = {
   entrypoint?: string;
   rollDownOpts: RolldownOptions;
   noMergeDefaults?: boolean;
+  noSingleFile?: boolean;
 };
 
-async function genRolldownOptions(packages: PackageOptions[]) {
-  const output: { label: string; value: object }[] = [];
-  for (const pac of packages) {
-    const isString = typeof pac === "string";
-    const packageName = isString ? pac : pac.name;
-    const packagePath = await assertPath(join(packagesDir, packageName));
-    const entrypoint = (!isString && pac.entrypoint)
-      ? pac.entrypoint
-      : "src/index.ts";
-    const input = await assertPath(join(packagePath, entrypoint));
+async function buildWithRolldown() {
+  const buildTargets = await multiselect({
+    message: "Choose the packages to build:",
+    options: await genMultiRolldownOptions(packagesList),
+    required: false,
+  });
 
-    const defaults = isString || !pac?.noMergeDefaults
-      ? {
-        input,
-        output: {
-          file: join(packagePath, "dist/index.js"),
-        },
-      }
-      : {};
-
-    const rollDownOpts = isString ? {} : pac.rollDownOpts;
-
-    output.push({
-      label: isString ? pac : pac?.label ?? pac.name,
-      value: { ...defaults, ...rollDownOpts },
-    });
+  if (!Array.isArray(buildTargets) || !buildTargets.length) {
+    cancel("Operation canceled.");
+    process.exit(1);
   }
 
+  await build([...buildTargets]);
+}
+
+async function genRolldownOptions(packageOptions: PackageOptions) {
+  const isString = typeof packageOptions === "string";
+  const packageName = isString ? packageOptions : packageOptions.name;
+  const packagePath = await assertPath(join(packagesDir, packageName));
+  const entrypoint = (!isString && packageOptions.entrypoint)
+    ? packageOptions.entrypoint
+    : "src/index.ts";
+  const input = await assertPath(join(packagePath, entrypoint));
+  const outputAsDir = !isString && packageOptions.noSingleFile;
+  const buildOutput = outputAsDir
+    ? { dir: join(packagePath, "dist") }
+    : { file: join(packagePath, "dist/index.js") };
+
+  const defaults = isString || !packageOptions?.noMergeDefaults
+    ? {
+      input,
+      output: buildOutput,
+    }
+    : {};
+
+  const rollDownOpts = isString ? {} : packageOptions.rollDownOpts;
+
+  return {
+    label: isString
+      ? packageOptions
+      : packageOptions?.label ?? packageOptions.name,
+    value: {
+      ...defaults,
+      ...rollDownOpts,
+    },
+  };
+}
+
+async function genMultiRolldownOptions(packages: PackageOptions[]) {
+  const output: { label: string; value: object }[] = [];
+  for (const pac of packages) {
+    const packageOpt = await genRolldownOptions(pac);
+    output.push(packageOpt);
+  }
   return output;
 }
+
+await buildWithRolldown();
