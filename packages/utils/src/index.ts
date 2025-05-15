606f7e5a3c143eb6104ddb0e879e145085418107
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2025-05-15 11:59:32 +0200

    Renamed a few files

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -6,7 +6,7 @@ export * from './prompts.js';
 export * from './arktype.js';
 export * from './error_handling/error_handling.js';
 export * from './testing/fs_handlers.js';
-export * from './testing/checks.js';
+export * from './testing/testing_helpers.js';
 export * from './strings/regex.js';
 export * from './objects/objects.js';
 export * from './arrays/arrays.js';
